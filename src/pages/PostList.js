ef6e1257063a057c9e1f79831cd0d28dbe0aaa73
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2021-07-07 18:25:28 +0900

    :wrench: Add mainpage(PostList.js)

---
+++ src/pages/PostList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PostList = (props) => {
+  return (
+    <React.Fragment>
+      포스트리스트
+    </React.Fragment>
+  )
+}
+
+export default PostList;
---
+++ src/shared/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      매거진 만들기!
+    </div>
+  );
+}
+
+export default App;
