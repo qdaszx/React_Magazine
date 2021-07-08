004fc82c51c23efa61f7ca450a441511740f9fca
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2021-07-08 18:05:04 +0900

    :sparkles: Add SignUp

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -4,8 +4,26 @@ import Grid from "../elements/Grid";
 import Input from "../elements/Input";
 import Button from "../elements/Button";
 
+import { useDispatch } from "react-redux";
+import { actionCreators as userActions } from "../redux/modules/user";
+
 const SignUp = (props) => {
-  const {} = props;
+  const dispatch = useDispatch();
+  const [id, setId] = React.useState("");
+  const [pwd, setPwd] = React.useState("");
+  const [pwd_check, setPwdCheck] = React.useState("");
+  const [user_name, setUserName] = React.useState("");
+  const signup = () => {
+    if (id === "" || pwd === "" || user_name === "") {
+      return;
+    }
+
+    if (pwd !== pwd_check) {
+      return;
+    }
+
+    dispatch(userActions.signupFB(id, pwd, user_name));
+  };
   return (
     <Grid bg="#EEEDE7" padding="16px">
       <Text size="30px" bold>
@@ -13,18 +31,39 @@ const SignUp = (props) => {
       </Text>
 
       <Grid padding="16px 0px">
-        <Input label="아이디" placeholder="아이디를 입력해주세요" />
+        <Input
+          label="아이디"
+          placeholder="아이디를 입력해주세요"
+          _onChange={(e) => {
+            setId(e.target.value);
+          }}
+        />
       </Grid>
       <Grid padding="16px 0px">
-        <Input label="닉네임" placeholder="닉네임를 입력해주세요" />
+        <Input
+          label="닉네임"
+          placeholder="닉네임를 입력해주세요"
+          _onChange={(e) => {
+            setUserName(e.target.value);
+          }}
+        />
       </Grid>
       <Grid padding="16px 0px">
-        <Input label="비밀번호" placeholder="비밀번호를 입력해주세요" />
+        <Input
+          label="비밀번호"
+          placeholder="비밀번호를 입력해주세요"
+          _onChange={(e) => {
+            setPwd(e.target.value);
+          }}
+        />
       </Grid>
       <Grid padding="16px 0px">
         <Input
           label="비밀번호 확인"
           placeholder="비밀번호를 다시 입력해주세요"
+          _onChange={(e) => {
+            setPwdCheck(e.target.value);
+          }}
         />
       </Grid>
       <Grid margin="20px 0px">
@@ -32,6 +71,7 @@ const SignUp = (props) => {
           text="회원가입하기"
           _onClick={() => {
             console.log("회원가입 완료");
+            signup();
           }}
         />
       </Grid>
--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -1,16 +1,18 @@
 import { createAction, handleActions } from "redux-actions";
 import { produce } from "immer";
 import { setCookie, getCookie, deleteCookie } from "../../shared/Cookie";
+import { auth } from "../../shared/firebase";
 
 // actions
-const LOG_IN = "LOG_IN";
 const LOG_OUT = "LOG_OUT";
 const GET_USER = "GET_USER";
+const SET_USER = "SET_USER";
 
 // action creators
-const logIn = createAction(LOG_IN, (user) => ({ user }));
+
 const logOut = createAction(LOG_OUT, (user) => ({ user }));
 const getUser = createAction(GET_USER, (user) => ({ user }));
+const setUser = createAction(SET_USER, (user) => ({ user }));
 
 const initialState = {
   user: null,
@@ -21,14 +23,49 @@ const initialState = {
 const loginAction = (user) => {
   return function (dispatch, getState, { history }) {
     console.log(history);
-    dispatch(logIn(user));
+    dispatch(setUser(user));
     history.push("/");
   };
 };
 
+const signupFB = (id, pwd, user_name) => {
+  return function (dispatch, getState, { history }) {
+    auth
+      .createUserWithEmailAndPassword(id, pwd)
+      .then((user) => {
+        console.log(user);
+
+        auth.currentUser
+          .updateProfile({
+            displayName: user_name,
+          })
+          .then(() => {
+            dispatch(
+              setUser({ user_name: user_name, id: id, user_profile: "" })
+            );
+            history.push("/");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+
+        // Signed in
+        // ...
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        console.log(errorCode, errorMessage);
+        // ..
+      });
+  };
+};
+
+// reducer
 export default handleActions(
   {
-    [LOG_IN]: (state, action) =>
+    [SET_USER]: (state, action) =>
       produce(state, (draft) => {
         setCookie("is_login", "success");
         draft.user = action.payload.user;
@@ -47,10 +84,10 @@ export default handleActions(
 
 // action creator export
 const actionCreators = {
-  logIn,
   getUser,
   logOut,
   loginAction,
+  signupFB,
 };
 
 export { actionCreators };
